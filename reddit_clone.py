7176be091ab8361707218d1d8125bee9904ac7f4
Aarib Abdullah <55854735+Aarib-Abdullah@users.noreply.github.com>
2020-03-07 14:23:15 -0800

    Made changes to dict function

--- reddit_clone.py
+++ reddit_clone.py
@@ -4,12 +4,41 @@ import sqlite3
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+def list_dicts_factory(cursor, row):
+    list = {}
+    number = 0   
+    d = {}
+    for index, col in enumerate(cursor.description):
+        
+        if col[0] in d:
+            list[number] = d
+            number += 1
+            d = {}
+
+        d[col[0]] = row[index]
+
+    list[number] = d
+
+    return list
+
 def dict_factory(cursor, row):
     d = {}
     for index, col in enumerate(cursor.description):
         d[col[0]] = row[index]
 
     return d
+
+@app.route('/r/posts/<int:post_id>/comments', methods = ['GET'])
+def retrive_post_CONTENT(post_id):
+    # pass
+    conn = sqlite3.connect('otaku.db')
+    conn.row_factory = list_dicts_factory
+    cur = conn.cursor()
+    all_posts = cur.execute('SELECT * FROM Posts  INNER JOIN comments \
+        ON Posts.post_id = comments.post_id WHERE Posts.post_id = {};'.format(post_id)).fetchall()
+
+    return jsonify(all_posts)
+    
 
 @app.route('/r/all', methods=['GET'])
 def home():
