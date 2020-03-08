61c82300f55602eb83c2addadb0dc69318eafeba
Aarib Abdullah <55854735+Aarib-Abdullah@users.noreply.github.com>
2020-03-07 18:33:41 -0800

    Add files via upload

--- reddit_clone.py
+++ reddit_clone.py
@@ -1,12 +1,23 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request, render_template, redirect, url_for
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route('/LogIN', methods=['POST', 'GET'])
+def login(): # The code is similar to https://flask.palletsprojects.com/en/1.1.x/quickstart/
+    #  if request.method == 'POST':
+    #      if valid_login(request.form['UserName'],
+    #                     request.form['Psswrd']):
+    #         return lo
+    pass
+
 def list_dicts_factory(cursor, row):
     list = {}
+    # list = dictionary_obj
     number = 0   
+    # number = start_from
     d = {}
     for index, col in enumerate(cursor.description):
         
@@ -28,10 +39,31 @@ def dict_factory(cursor, row):
 
     return d
 
-@app.route('/submit', methods= ['GET', 'POST'])
-def preparing_post():
-    pass
 
+def insert_into_posts(conn, task): #https://www.sqlitetutorial.net/sqlite-python/insert/
+    sql = ''' INSERT INTO Posts(Owner_ID, owner_name,post_id, post_title, post_body, upvotes, downvotes, date)
+                    VALUES(?,?,?,?,?,?,?,?) ;'''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    return cur.lastrowid
+
+@app.route('/submit', methods = ['GET', 'POST'])
+def preparing_reddite_post():
+    if request.method == 'GET':
+        return render_template('submit.html')
+    elif request.method == 'POST':
+        conn = sqlite3.connect('otaku.db')
+        date_stamp = str(datetime.utcnow())
+        post_title = request.form['post-title']
+        post_body = request.form['post-body']
+        tupl = (9000, 'Filthy Frank',1200, post_title, post_body, 1, 0, date_stamp)
+
+        with conn:
+            row_id = insert_into_posts(conn, tupl)
+   
+        conn.close()
+        # return jsonify(all_posts)
+    return redirect(url_for('retrive_post_CONTENT', post_id = 1200))
 
 
 @app.route('/r/posts/<int:post_id>/comments', methods = ['GET'])
@@ -40,10 +72,22 @@ def retrive_post_CONTENT(post_id):
     conn = sqlite3.connect('otaku.db')
     conn.row_factory = list_dicts_factory
     cur = conn.cursor()
-    all_posts = cur.execute('SELECT * FROM Posts  INNER JOIN comments \
-        ON Posts.post_id = comments.post_id WHERE Posts.post_id = {};'.format(post_id)).fetchall()
+    # all_posts = cur.execute('SELECT * FROM Posts  INNER JOIN comments \
+    #     ON Posts.post_id = comments.post_id WHERE Posts.post_id = {};'.format(post_id)).fetchall()
+    post_content = cur.execute('SELECT * FROM Posts  \
+         WHERE Posts.post_id = {};'.format(post_id)).fetchall()
+    
+    comments = cur.execute('SELECT * FROM comments  \
+         WHERE comments.post_id = {};'.format(post_id)).fetchall()
+    # conn.commit()
+    conn.close()
 
-    return jsonify(all_posts)
+    whole_dict ={}
+    whole_dict["post"] = post_content
+    whole_dict["comments"] = comments
+
+
+    return jsonify(whole_dict)
     
 
 @app.route('/r/all', methods=['GET'])
@@ -53,7 +97,8 @@ def home():
     conn.row_factory = dict_factory
     cur = conn.cursor()
     all_posts = cur.execute('SELECT * FROM Posts;').fetchall()
-
+    conn.commit()
+    conn.close()
 
 
     return jsonify(all_posts)
@@ -66,27 +111,6 @@ def home():
 @app.errorhandler(404)
 def page_not_found(e):
     return '''<h1>404 NOT FOUND (i think)</h1><p>{}</p>'''.format(e), 404
-
-
-
-@app.route('/u/<userid>')
-def user():
-    pass
-
-@app.route('/r/<community>/')
-def community():
-    pass
-
-@app.route('/r/<community>/posts/<post_id>/comments')
-def post():
-    pass
-
-@app.route('/r/<community>/<post_id>/comments/<comment_id>')
-def comment():
-    pass
-
-
-
 
 
 app.run()
--- somestuff.py
+++ somestuff.py
@@ -1,42 +1,15 @@
 import sqlite3
-from flask import jsonify, Flask
+from flask import jsonify
 
-app = Flask(__name__)
+conn = sqlite3.connect("otaku.db")
 
-# conn = sqlite3.connect("otaku.db")
-
-# cursor = conn.cursor()
+cursor = conn.cursor()
 
 
-# for i in cursor.execute("SELECT * FROM Posts;"):
-#     print(i)
-#     list.append(i)
+for i in cursor.execute("SELECT * FROM Posts;"):
+    print(i)
+    list.append(i)
 
-# print(str(list))
-
-
-@app.route('/')
-def default():
-    return "Hello Doe"
-
-@app.route('/u/<userid>')
-def user():
-    pass
-
-@app.route('/r/<community>')
-def community():
-    pass
-
-@app.route('/r/<community>/<post>/')
-def post():
-    pass
-
-@app.route('/r/<community>/<post>/>comment>/')
-def comment():
-    pass
-
-
-if __name__ == '__main__':
-    app.run()
+print(str(list))
 
 
