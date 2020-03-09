9e2f98e868afac1ffbe671a816f3e56a477afc8b
Aarib Abdullah <55854735+Aarib-Abdullah@users.noreply.github.com>
2020-03-09 11:48:01 -0700

    fixed Sql insert command, but still some issues

--- reddit_clone.py
+++ reddit_clone.py
@@ -5,6 +5,24 @@ from datetime import datetime
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+def Connect_to_db(need_dicts_factory = False):
+    conn = sqlite3.connect('otaku.db')
+    if need_dicts_factory is True:
+        conn.row_factory = dict_factory
+    else:
+        conn.row_factory = list_dicts_factory
+    cur = conn.cursor()
+    return conn, cur
+
+def Close_db(conn, cur):
+    try:
+        cur.close()
+        conn.close()
+    except:
+        return False
+    
+    return True
+
 @app.route('/LogIN', methods=['POST', 'GET'])
 def login(): # The code is similar to https://flask.palletsprojects.com/en/1.1.x/quickstart/
     #  if request.method == 'POST':
@@ -13,28 +31,32 @@ def login(): # The code is similar to https://flask.palletsprojects.com/en/1.1.x
     #         return lo
     pass
 
+#CHANGE TO ONLY GIVE A DICT (UPDATE: THIS IS NOT NEEDED ANYMORE)
 def list_dicts_factory(cursor, row):
-    list = []
-    # list = {}
+    # list = []
+    list = {}
     # list = dictionary_obj
     # number = 0   
     # number = start_from
     d = {}
     for index, col in enumerate(cursor.description):
         
-        if col[0] in d:
+        # if col[0] in d:
             # list[number] = d
-            list.append(d)
+            # list.append(d)
             # number += 1
             # d = {}
 
         d[col[0]] = row[index]
 
     # list[number] = d
-    list.append(d)
+    # list.append(d)
+    # list['result'] = d
 
     return list
 
+
+#USE this only
 def dict_factory(cursor, row):
     d = {}
     for index, col in enumerate(cursor.description):
@@ -43,9 +65,11 @@ def dict_factory(cursor, row):
     return d
 
 
-
+#CHANGE THE QUERY
 def insert_into_posts(conn, task): #https://www.sqlitetutorial.net/sqlite-python/insert/
-    sql = ''' INSERT INTO Posts(Owner_ID, owner_name,post_id, post_title, post_body, upvotes, downvotes, date)
+    #must change the parameter feilds, they notw
+    #NOW THEY ARE CHANGED
+    sql = ''' INSERT INTO Posts(Owner_ID, owner_name,post_id, post_title, post_body, upvotes, downvotes, date, comm_name, karma)
                     VALUES(?,?,?,?,?,?,?,?) ;'''
     cur = conn.cursor()
     cur.execute(sql, task)
@@ -59,74 +83,136 @@ def killiing_post(post_id):
     connection.commit()
     cur.execute("DELETE FROM comments WHERE comments.post_id={};".format(post_id))
     connection.commit()
-    # connection.execute("DELETE FROM Posts WHERE Posts.post_id={};".format(post_id))
-    # connection.execute("DELETE FROM comments WHERE comments.post_id={};".format(post_id))
+
     cur.close()
     connection.close()
     return redirect(url_for('home'))
 
-@app.route('/submit', methods = ['GET', 'POST'])
-def preparing_reddite_post():
+#CHANGE TUPLE FIELD
+@app.route('/r/<community>/submit', methods = ['GET', 'POST'])
+def preparing_reddite_post(community):
     if request.method == 'GET':
         return render_template('submit.html')
     elif request.method == 'POST':
-        conn = sqlite3.connect('otaku.db')
+        conn, cur = Connect_to_db()
+        data = request.get_json()
         date_stamp = str(datetime.utcnow())
-        post_title = request.form['post-title']
-        post_body = request.form['post-body']
-        tupl = (9000, 'Filthy Frank',1200, post_title, post_body, 1, 0, date_stamp)
+        # post_title = request.form['post-title']
+        # post_body = request.form['post-body']
+        post_title = data['post_title']
+        post_body = data['post_body']
+        post_id = data['post_id']
+        owner_id = data['owner_id']
+        owner_name = data['owner_name']
+        #INCLUDE DATABASE VARIABLES IN THIS TUPLE (UPDATE, DID IT)
+        # tupl = (9000, 'Filthy Frank',1200, post_title, post_body, 1, 0, date_stamp, community, 1)
+        tupl = (owner_id, owner_name, post_id, post_title, post_body, 1,0, date_stamp , community, 1)
 
         with conn:
             row_id = insert_into_posts(conn, tupl)
-   
-        conn.close()
-        # return jsonify(all_posts)
+
+        Close_db(conn=conn, cur=cur)
     return redirect(url_for('retrive_post_CONTENT', post_id = 1200))
 
+def vote(community, post_id, is_increment):
+    conn, cur = Connect_to_db()
+    if is_increment:
+        cur.execute("UPDATE Posts \
+                 SET upvotes = upvotes + 1 , karma = karma + 1\
+                 WHERE comm_name = '{}' AND post_id = {};".format(community,post_id))
 
-@app.route('/r/posts/<int:post_id>/comments', methods = ['GET'])
-def retrive_post_CONTENT(post_id):
-    # pass
-    conn = sqlite3.connect('otaku.db')
-    conn.row_factory = list_dicts_factory
-    cur = conn.cursor()
-    # all_posts = cur.execute('SELECT * FROM Posts LEFT JOIN comments \
-    #     ON Posts.post_id = comments.post_id  WHERE Posts.post_id = {};'.format(post_id)).fetchall()
-    post_content = cur.execute('SELECT * FROM Posts  \
-         WHERE Posts.post_id = {};'.format(post_id)).fetchall()
+    else:
+        cur.execute("UPDATE Posts \
+                SET downvotes = downvotes + 1 , karma = karma - 1\
+                WHERE comm_name = '{}' AND post_id = {};".format(community,post_id))
+
+    conn.commit()
+    Close_db(conn, cur)
+
+@app.route('/r/<community>/posts/<int:post_id>/karma/increment', methods = ['PUT']) #must find way to send status code '200'
+def increment_post(community, post_id): 
+    vote(community=community, post_id=post_id, is_increment=True)
+    return redirect(url_for('retrive_post_CONTENT', community = community, post_id = post_id))
     
+@app.route('/r/<community>/posts/<int:post_id>/karma/decrement', methods = ['PUT']) #must find way to send status code '200'
+def decrement_post(community, post_id): 
+    vote(community=community, post_id=post_id, is_increment=False)
+    return redirect(url_for('retrive_post_CONTENT', community = community, post_id = post_id))
+
+@app.route('/r/<community>/posts/<int:post_id>/comments', methods = ['GET'])
+def retrive_post_CONTENT(community, post_id):
+    conn,cur = Connect_to_db(need_dicts_factory=True)
+    post_content = cur.execute("SELECT * FROM Posts  \
+         WHERE Posts.post_id = {} AND Posts.comm_name = '{}';".format(post_id, community)).fetchall()
     comments = cur.execute('SELECT * FROM comments  \
          WHERE comments.post_id = {} ORDER BY date DESC;'.format(post_id)).fetchall()
-    # conn.commit()
-    conn.close()
+    Close_db(conn=conn, cur=cur)
 
     whole_dict ={}
-    # whole_dict["post"] = post_content[0]
-    # whole_dict["comments"] = comments
     whole_dict["post"] = post_content[0]
-    whole_dict['comments'] = {}
-    for i in range(len(comments)):
-        whole_dict['comments'][i] = comments[i]
-
+    whole_dict['comments'] = comments
     return jsonify(whole_dict)
-    # return jsonify(all_posts)
+
     
 
+
+@app.route('/r/<community>', methods = ["GET"])
+def commmunity_posts(community):
+    conn, cur = Connect_to_db(need_dicts_factory=True)
+    all_community_posts = cur.execute("SELECT * FROM Posts \
+                                       WHERE Posts.comm_name = '{}' \
+                                       ORDER BY date DESC \
+                                       LIMIT 20;".format(community)).fetchall()    
+    Close_db(conn, cur)
+    return jsonify(all_community_posts)
+    
+    
 @app.route('/r/all', methods=['GET'])
+def topPosts():
+    conn, cur = Connect_to_db(need_dicts_factory=True)
+    all_posts = cur.execute('SELECT * FROM Posts \
+                             ORDER BY karma DESC \
+                             LIMIT 20;').fetchall()
+
+    Close_db(conn, cur)
+    results = {}
+    results['posts'] = all_posts
+    return jsonify(results)
+
+@app.route('/', methods=['GET'])
 def home():
     #shows post
-    conn = sqlite3.connect('otaku.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_posts = cur.execute('SELECT * FROM Posts;').fetchall()
-    conn.commit()
-    conn.close()
+    conn, cur = Connect_to_db()
+    all_posts = cur.execute('SELECT * FROM Posts \
+                             ORDER BY date DESC \
+                             LIMIT 20;').fetchall()
+    Close_db(conn, cur)
 
 
     return jsonify(all_posts)
     # return '''<h1>A weeb's sad tale</h1>
     # <p>Vader: Your feelings for your waifu are not real<br>
     # StarKiller: THEY ARE TO ME!</p>'''
+
+@app.route('/sort', methods = ['GET'])
+def sort_post_id():
+    
+    list = request.args['list']
+    list = list.split(',')
+    print(list[0])
+    for i in range(0, len(list)):
+        list[i] = int(list[i])
+    list = top_posts_from_list(list)
+    return list
+
+
+def top_posts_from_list(someList):
+    conn, cur = Connect_to_db(need_dicts_factory=True)
+    top_scoring_posts = cur.execute("SELECT * FROM Posts \
+                                     WHERE post_id IN {} \
+                                     ORDER BY karma DESC;".format(tuple(someList))).fetchall()
+    Close_db(conn, cur)
+    return jsonify(top_scoring_posts)
 
 
 
@@ -136,3 +222,6 @@ def page_not_found(e):
 
 
 app.run()
+
+# print(top_posts_from_list([1200, 787]))
+
--- somestuff.py
+++ somestuff.py
@@ -1,15 +1,41 @@
 import sqlite3
-from flask import jsonify
+# from flask import jsonify
+
 
 conn = sqlite3.connect("otaku.db")
 
 cursor = conn.cursor()
 
 
-for i in cursor.execute("SELECT * FROM Posts;"):
-    print(i)
-    list.append(i)
+# for i in cursor.execute("SELECT * FROM Posts;"):
+#     print(i)
+#     list.append(i)
 
-print(str(list))
+# print(str(list))
+
+def dict_factory(cursor, row):
+    d = {}
+    for index, col in enumerate(cursor.description):
+        d[col[0]] = row[index]
+
+    return d
+
+def top_posts_from_list(someList):
+    # conn, cur = Connect_to_db(need_dicts_factory=True)
+    conn = sqlite3.connect('otaku.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    top_scoring_posts = cur.execute("SELECT * FROM Posts \
+                                     WHERE post_id IN {} \
+                                     ORDER BY karma DESC;".format(tuple(someList))).fetchall()
+    # Close_db(conn, cur)
+    cur.close()
+    conn.close()
+
+    return top_scoring_posts
 
 
+
+var=top_posts_from_list([1200, 787])
+
+print(var)
